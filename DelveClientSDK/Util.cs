90df6238409c1b8edf622e000062cceea42c79b5
Mohammad Dashti <mdashti@gmail.com>
2020-08-13 05:13:38 -0700

    [ClientSDKs]/C#] Added some utility functions

---
+++ DelveClientSDK/Util.cs
@@ -0,0 +1,90 @@
+
+using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Com.RelationalAI
+{
+    public static class EnumString
+    {
+        public static string GetDescription<T>(this T enumerationValue)
+            where T : struct
+        {
+            Type type = enumerationValue.GetType();
+            if (!type.IsEnum)
+            {
+                throw new ArgumentException("EnumerationValue must be of Enum type", "enumerationValue");
+            }
+
+            //Tries to find a DescriptionAttribute for a potential friendly name
+            //for the enum
+            MemberInfo[] memberInfo = type.GetMember(enumerationValue.ToString());
+            if (memberInfo != null && memberInfo.Length > 0)
+            {
+                object[] attrs = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attrs != null && attrs.Length > 0)
+                {
+                    //Pull out the description value
+                    return ((DescriptionAttribute)attrs[0]).Description;
+                }
+            }
+            //If we have no description attribute, just return the ToString of the enum
+            return enumerationValue.ToString();
+        }
+    }
+
+    public static class ByteArrayExtensionMethods
+    {
+        public static string ToHex(this byte[] data)
+        {
+            return ToHex(data, "");
+        }
+        public static string ToHex(this byte[] data, string prefix)
+        {
+            char[] lookup = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+            int i = 0, p = prefix.Length, l = data.Length;
+            char[] c = new char[l * 2 + p];
+            byte d;
+            for (; i < p; ++i) c[i] = prefix[i];
+            i = -1;
+            --l;
+            --p;
+            while (i < l)
+            {
+                d = data[++i];
+                c[++p] = lookup[d >> 4];
+                c[++p] = lookup[d & 0xF];
+            }
+            return new string(c, 0, c.Length);
+        }
+        public static byte[] FromHex(this string str)
+        {
+            return FromHex(str, 0, 0, 0);
+        }
+        public static byte[] FromHex(this string str, int offset, int step)
+        {
+            return FromHex(str, offset, step, 0);
+        }
+        public static byte[] FromHex(this string str, int offset, int step, int tail)
+        {
+            byte[] b = new byte[(str.Length - offset - tail + step) / (2 + step)];
+            byte c1, c2;
+            int l = str.Length - tail;
+            int s = step + 1;
+            for (int y = 0, x = offset; x < l; ++y, x += s)
+            {
+                c1 = (byte)str[x];
+                if (c1 > 0x60) c1 -= 0x57;
+                else if (c1 > 0x40) c1 -= 0x37;
+                else c1 -= 0x30;
+                c2 = (byte)str[++x];
+                if (c2 > 0x60) c2 -= 0x57;
+                else if (c2 > 0x40) c2 -= 0x37;
+                else c2 -= 0x30;
+                b[y] = (byte)((c1 << 4) + c2);
+            }
+            return b;
+        }
+    }
+}
