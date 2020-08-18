7e7c8b60cfb9b7582da61ec27704f10422c5de5b
Mohammad Dashti <mdashti@gmail.com>
2020-08-17 22:44:04 -0700

    [ClientSDKs/C#] Separated Connection from CloudConnection

--- DelveClientSDK/Connection.cs
+++ DelveClientSDK/Connection.cs
@@ -17,21 +17,13 @@ namespace Com.RelationalAI
             TransactionMode defaultOpenMode = DEFAULT_OPEN_MODE,
             string scheme = DEFAULT_SCHEME,
             string host = DEFAULT_HOST,
-            int port = DEFAULT_PORT,
-            RAIInfra infra = DEFAULT_INFRA,
-            RAIRegion region = DEFAULT_REGION,
-            RAICredentials creds = null,
-            bool verifySSL = DEFAULT_VERIFY_SSL)
+            int port = DEFAULT_PORT)
         {
             this.dbname = dbname;
             this.defaultOpenMode = defaultOpenMode;
             this.scheme = scheme;
             this.host = host;
             this.port = port;
-            this.infra = infra;
-            this.region = region;
-            this.creds = creds;
-            this.verifySSL = verifySSL;
         }
 
         public string dbname { get; }
@@ -39,13 +31,53 @@ namespace Com.RelationalAI
         public string scheme { get; }
         public string host { get; }
         public int port { get; }
-        public RAIInfra infra { get; }
-        public RAIRegion region { get; }
-        public RAICredentials creds { get; }
-        public bool verifySSL { get; }
+        public virtual RAIInfra infra { get{ return DEFAULT_INFRA; } }
+        public virtual RAIRegion region { get{ return DEFAULT_REGION; } }
+        public virtual RAICredentials creds { get{ return null; } }
+        public virtual bool verifySSL { get{ return DEFAULT_VERIFY_SSL; } }
+        public virtual string computeName { get{ return null; } }
 
         public Uri baseUrl {
             get { return new UriBuilder(this.scheme, this.host, this.port).Uri; }
+        }
+    }
+
+    public class CloudConnection : Connection
+    {
+        public override RAIInfra infra { get; }
+        public override RAIRegion region { get; }
+        public override RAICredentials creds { get; }
+        public override bool verifySSL { get; }
+        public override string computeName { get; }
+
+        public CloudConnection(
+            string dbname,
+            TransactionMode defaultOpenMode = DEFAULT_OPEN_MODE,
+            string scheme = DEFAULT_SCHEME,
+            string host = DEFAULT_HOST,
+            int port = DEFAULT_PORT,
+            RAIInfra infra = DEFAULT_INFRA,
+            RAIRegion region = DEFAULT_REGION,
+            RAICredentials creds = null,
+            bool verifySSL = DEFAULT_VERIFY_SSL,
+            string computeName = null
+        ) : base(dbname, defaultOpenMode, scheme, host, port)
+        {
+            this.infra = infra;
+            this.region = region;
+            this.creds = creds;
+            this.verifySSL = verifySSL;
+            this.computeName = computeName;
+        }
+        public CloudConnection(
+            Connection conn,
+            RAIInfra infra = DEFAULT_INFRA,
+            RAIRegion region = DEFAULT_REGION,
+            RAICredentials creds = null,
+            bool verifySSL = DEFAULT_VERIFY_SSL,
+            string computeName = null
+        ) : this(conn.dbname, conn.defaultOpenMode, conn.scheme, conn.host, conn.port, infra, region, creds, verifySSL, computeName)
+        {
         }
     }
 }
