5cb2ba5ac4b15e37e525a33b502a81e14bdc6164
Helmi <helmiinour@gmail.com>
2020-08-21 17:29:28 +0100

    adding remain management api actions.

--- DelveClientSDK/DelveCloudClient.cs
+++ DelveClientSDK/DelveCloudClient.cs
@@ -34,8 +34,46 @@ namespace Com.RelationalAI
 
         public ListComputesResponseProtocol listComputes() {return this.ComputeGetAsync().Result;}
 
+        public CreateComputeResponseProtocol createCompute(string displayName, string size, bool dryRun)
+        {
+            CreateComputeRequestProtocol request = new CreateComputeRequestProtocol();
+            request.Region = EnumString.GetDescription(conn.region);
+            request.Display_name = displayName;
+            request.Size = size;
+            request.Dryrun = dryRun;
+            return this.ComputePutAsync(request).Result;
+        }
+
+        public DeleteComputeResponseProtocol deleteCompute(string computeName, bool dryRun)
+        {
+            DeleteComputeRequestProtocol request = new DeleteComputeRequestProtocol();
+            request.Compute_name = computeName;
+            request.Dryrun = dryRun;
+            return this.ComputeDeleteAsync(request).Result;
+        }
+
         public ListDatabasesResponseProtocol listDatabases() {return this.DatabaseGetAsync().Result;}
 
+        public void updateDatabase(string displayName, string defaultComputeName, bool removeDefaultCompute, bool dryRun)
+        {
+            UpdateDatabaseRequestProtocol request = new UpdateDatabaseRequestProtocol();
+            request.Display_name = displayName;
+            request.Default_compute_name = defaultComputeName;
+            request.Remove_default_compute = removeDefaultCompute;
+            request.Dryrun = dryRun;
+            this.DatabasePostAsync(request).Wait();
+        }
         public ListUsersResponseProtocol listUsers() {return this.UserGetAsync().Result;}
+
+        public CreateUserResponseProtocol createUser(string username, string firstName, string lastName, string email, bool dryRun)
+        {
+            CreateUserRequestProtocol request = new CreateUserRequestProtocol();
+            request.Username = username;
+            request.First_name = firstName;
+            request.Last_name = lastName;
+            request.Email = email;
+            request.Dryrun = dryRun;
+            return this.UserPutAsync(request).Result;
+        }
     }
 }
