f466982e77306c24b8d3ddc97af4cd2abd4d618d
Mohammad Dashti <mdashti@gmail.com>
2020-10-05 09:29:35 -0700

    [ClientSDKs/C#] Fixed a test

--- DelveClientSDKTests/IntegrationTestsCommons.cs
+++ DelveClientSDKTests/IntegrationTestsCommons.cs
@@ -66,7 +66,7 @@ namespace Com.RelationalAI
             // create_database
             // =============================================================================
             Assert.IsTrue(conn.CreateDatabase(overwrite: true));
-            Assert.Throws<AggregateException>(() => conn.CreateDatabase());
+            Assert.Throws<Exception>(() => conn.CreateDatabase());
 
             // install_source
             // =============================================================================
